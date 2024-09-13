3f38c14dde76d657acb036d408cad0223e904d24
xpydrrloki <vvincent911@gmail.com>
2024-09-13 20:55:22 +0700

    fix: page header change

--- apps/web/src/app/(root)/attendance/page.tsx
+++ apps/web/src/app/(root)/attendance/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Attendance = () => {
   return (
-    <div>Attendance</div>
-  )
-}
+    <main className="container mx-auto px-4">
+      <h2 className="mx-auto mb-4 max-w-6xl text-2xl font-bold">Presensi & Lembur</h2>
+    </main>
+  );
+};
 
-export default Attendance
+export default Attendance;
--- apps/web/src/app/(root)/employees/page.tsx
+++ apps/web/src/app/(root)/employees/page.tsx
@@ -46,7 +46,7 @@ const Employees = () => {
   }, [searchParams]);
   return (
     <main className="container px-8 py-12">
-      <h2 className="mx-auto mb-4 max-w-6xl text-2xl font-bold">Employees</h2>
+      <h2 className="mx-auto mb-4 max-w-6xl text-2xl font-bold">Data Karyawan</h2>
       <div className="container mx-auto mb-10 min-h-[640px] max-h-[720px] min-w-[840px] max-w-6xl border-2 bg-white py-4 shadow-xl flex flex-col justify-between">
         {isLoading || !data ? (
           <div>Loading...</div>
